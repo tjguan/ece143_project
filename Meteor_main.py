887d8f38a53144bb164c7f0cece6cf507cd35815
AleksJere <130003125+AleksJere@users.noreply.github.com>
2023-11-22 21:15:09 -0800

    Create Meteor_main.py
    Creating main file

---
+++ Meteor_main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 22 21:11:11 2023
+
+@author: jerem
+"""
+import pandas as pd
+
+
+if __name__ == "__main__":
+    
+    file = csv_read("")
+
+
+
+
+
+
+
+
